60a43dd6c4376176747499f460d9a419f71c127f
Robert Stauss <robertrstauss@gmail.com>
2020-09-18 09:59:16 -0400

    working version

--- add-em-up.js
+++ add-em-up.js
@@ -7,26 +7,32 @@ function ask(questionText) {
   })
 }
 
-function adder() {
+async function adder() {
   let response = await ask("Let's add some numbers together!\nAre you ready? ")
 
-  while (response !== 'yes') {
+  while (response !== 'yes' && response !== '') {
     response = await ask('Are you ready now? ')
   }
 
   let number = await ask('What number are we starting with? ')
 
-  if(number = NaN) {
+  number = parseFloat(number)
+
+  if (isNaN(number)) {
     number = await ask("Let's try this again. Please enter a number. ")
-  } else {
-    let addThis = await ask("Keep entering numbers to add. Then type 'done' to see the result. ")
 
-    while (addThis !== 'done') {
-      number += addThis
-      
-      let addThis = await ask('Enter the next number, or "done" ')
-    }
-
-    console.log(number)
+    number = parseFloat(number)
   }
+  let addThis = await ask("Keep entering numbers to add. Then type 'done' to see the result. ")
+
+  while (addThis !== 'done') {
+    number += parseFloat(addThis)
+
+    addThis = await ask('Enter the next number, or "done" ')
+  }
+
+  console.log(number)
+  process.exit()
 }
+
+adder()
